283d6c827e1bce7dea31ebc534d8861f0ad8db16
afuedia <cholastal@gmail.com>
2025-10-19 19:34:44 +0100

    ya no se ni que hacer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://afuedia.github.io', //poner aquí el dominio
-  base: 'ascecca',
+  site: "https://afuedia.github.io", //poner aquí el dominio
+  base: "ascecca",
   image: {
     responsiveStyles: true,        
     layout: 'constrained'
   },
-  
   
 });
