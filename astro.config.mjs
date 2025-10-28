aa801e470d1aef412588ed0e8598788636dd0378
afuedia <cholastal@gmail.com>
2025-10-28 21:29:28 +0000

    Reestructura el contenido de la carpeta y configur alias para llamar a src/ con @/

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { fileURLToPath } from 'node:url';
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,5 +10,11 @@ export default defineConfig({
     responsiveStyles: true,        
     layout: 'constrained'
   },
-
+  vite: {
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    }
+  }
 });
